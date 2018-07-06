0ef9a4b7109540da0cc64d60f4dd40bebaa3979a
Jie Zhang <jzhang14@paypal.com>
2018-07-06 17:36:06 +0800

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
     FormGroup, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent'
+
 
 const required = (val) => val && val.length;
 const maxLenth = (len) => (val) => !(val) || (val.length <= len);
@@ -123,9 +125,23 @@ function RenderComments({comments, addComment, dishId}) {
 };
 
 const DishDetail = (props) => {
-    const dish = props.dish;
-
-    if (dish != null) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    } else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        )
+    } else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
