df32f1cf3edefb91300eab6745d90b09fba79fc3
Zhang, Jie <jzhang14@paypal.com>
2018-07-07 16:12:22 +0800

    remove unuse constructor

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,9 +25,6 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     componentDidMount() {
         this.props.fetchDishes();
